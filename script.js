63cbc3cc2b9d36e68aa624173851adb4f0005744
WinterBlant <45798119+WinterBlant@users.noreply.github.com>
2019-04-27 15:43:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,176 +1,178 @@
+var count = 0;
+var quote;
+var imgs;
+var loadedImgs;
+
+createCanv();
 main();
 
-var
-  quote,
-  imgs,
-  countLoadPctrs;
-
 function main() {
-  quote = null;
-  imgs = new Array();
-  countLoadPctrs = 0;
-  generateHTML();
-  start();
-}
-
-function generateHTML() {
-  var
-    canvas = document.createElement('canvas'),
-    body = document.getElementById('body'),
-    download = document.createElement('button'),
-    div = document.createElement('div');
-
-  body.style.backgroundColor = 'beige';
-
-  div.style.width = '100%';
-  div.style.display = 'flex';
-  div.style.flexDirection = 'column';
-  div.style.alignItems = 'center';
-
-  canvas.id = 'canvas';
-  canvas.width = 600;
-  canvas.height = 600;
-
-  download.style.visibility = 'hidden';
-  download.id = 'download';
-  download.style.margin = '20px';
-  download.style.padding = '10px';
-  download.innerHTML = 'Download';
-  download.style.backgroundColor = 'mediumpurple';
-  download.style.color = 'azure';
-  download.style.fontSize = '16px';
-  download.onclick =
-    function () {
-      var link = document.createElement('a');
-
-      link.href = canvas.toDataURL('image/jpg');
-      link.download = 'quote.jpg';
-      link.click();
-    };
-
-  div.appendChild(canvas);
-  div.appendChild(download);
-  body.appendChild(div);
-}
-
-function start() {
-  for (var i = 0; i < 4; i++) {
-    imgs[i] = new Image();
-    imgs[i].crossOrigin = 'anonymous';
-    a = (i + 5) * 100;
-    imgs[i].src = 'https://source.unsplash.com/' + a + 'x' + a + '/?mountain';
-    imgs[i].onload = function () {
-      countLoadPctrs++;
-      if (countLoadPctrs == 4) {
-        drawPctrs();
-      }
-    };
-  }
-}
-
-function drawPctr(img, sx, sy, swidth, sheight, x, y, width, height) {
-  var ctx = canvas.getContext('2d');
-
-  ctx.drawImage(img, sx, sy, swidth, sheight, x, y, width, height);
-}
-
-function calcCoords(img, width, height) {
-  if (width != height) {
-    if (width < height) {
-      return [img.naturalWidth * (0.5 - 0.5 / (height / width)), 0,
-      img.naturalWidth / (height / width), img.naturalHeight]
+    quote = null;
+    imgs = new Array();
+    loadedImgs = 0;
+    drawnImgs = 0;
+	download.style.visibility = 'hidden';
+	next.style.visibility = 'hidden';
+    for (var i = 0; i < 4; i++) {
+        imgs[i] = new Image();
+        imgs[i].crossOrigin = 'anonymous';
     }
-    else {
-      return [0, img.naturalHeight * (0.5 - 0.5 / (width / height)),
-        img.naturalWidth, img.naturalHeight / (width / height)]
-    }
-  }
-  else {
-    return [0, 0, img.naturalWidth, img.naturalHeight]
-  }
+    getPctrs();
 }
 
-function drawPctrs() {
-  var
-    w,
-    h,
-    x = 0,
-    y = 0,
-    ox = 150 + Math.round(Math.random() * 250),
-    oy = 150 + Math.round(Math.random() * 250),
-    h = oy,
-    parameters = [];
+function createCanv() {
+    var canvas = document.createElement('canvas');
+    var body = document.getElementById('body');
+    var download = document.createElement('button');
+    var next = document.createElement('button');
+    var block = document.createElement('div');
+	var btnblock = document.createElement('div');
 
-  for (var i = 0; i < 2; i++) {
-    w = ox;
-    parameters = calcCoords(imgs[i * 2], w, h);
-    drawPctr(imgs[i * 2], parameters[0], parameters[1], parameters[2], parameters[3], x, y, w, h);
-    x = ox;
-    w = 600 - w;
-    parameters = calcCoords(imgs[i * 2 + 1], w, h);
-    drawPctr(imgs[i * 2 + 1], parameters[0], parameters[1], parameters[2], parameters[3], x, y, w, h);
-    x = 0;
-    y = oy;
-    h = 600 - h;
-  }
+    block.style.width = '100%';
+    block.style.display = 'flex';
+    block.style.flexDirection = 'column';
+    block.style.alignItems = 'center';
 
-  var ctx = canvas.getContext('2d');
+    canvas.id = 'canvas';
+    canvas.width = 600;
+    canvas.height = 600;
+    canvas.style.outline = '4px black solid';
 
-  ctx.fillStyle = 'rgba(0,0,0,0.4)';
-  ctx.fillRect(0, 0, 600, 600);
-  getText();
+    download.id = 'download';
+    download.style.margin = '20px';
+    download.style.padding = '10px';
+    download.innerHTML = 'Download';
+    download.style.backgroundColor = 'green';
+    download.style.color = 'white';
+    download.style.fontSize = '16px';
+    download.onclick =
+        function () {
+            var link = document.createElement('a');
+            count++;
+            link.href = canvas.toDataURL('image/jpg');
+            link.download = 'quote' + count + '.jpg';
+            link.click();
+        };
+    next.id = 'next';
+    next.innerHTML = 'Next';
+    next.style.backgroundColor = 'yellow';
+    next.style.fontSize = '16px';
+    next.style.padding = '10px';
+	next.style.margin = '20px';
+    next.onclick =
+        function () {
+            main();
+        }
+    block.appendChild(canvas);
+	block.appendChild(btnblock);
+    btnblock.appendChild(download);
+    btnblock.appendChild(next);
+    body.appendChild(block);
 }
 
-async function getText() {
-  var http = new XMLHttpRequest;
-
-  http.open('GET', 'https://cors-anywhere.herokuapp.com/' +
-    'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru', true);
-  http.send();
-  http.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(http.responseText);
-      quote = JSON.parse(http.responseText)['quoteText'];
-      drawText();
-    }
-  }
+function getPctrs() {
+    $.ajax({
+        url: "https://api.codetabs.com/v1/proxy",
+        data: {
+            quest: 'https://api.unsplash.com/photos/random?' +
+                'client_id=e25bfbf8a1d462f0f2e05187bf2bc35a53e3b18bfc7430d17d48f5baadea9b5e' + '&' +
+                'count=4' + '&' + 'orientation=squarish'
+        }
+    })
+        .done(
+            function (data) {
+                for (var i = 0; i < 4; i++) {
+                    imgs[i].src = data[i].urls.small;
+                    imgs[i].onload = function () {
+                        loadedImgs++;
+						if (loadedImgs == 4) {
+							processPics();
+							}
+                    };
+                }
+            })
 }
 
-function formateStrings(context, text, x, y, maxWidth, lineHeight) {
-  var
-    words = text.split(" "),
-    countWords = words.length,
-    countStrs = Math.floor(context.measureText(text).width / 550),
-    str = "";
+function processPics() {
+        var
+            x = 0,
+            y = 0,
+            ox = 200 + Math.round(Math.random() * 200),
+            oy = 200 + Math.round(Math.random() * 200),
+            h = oy;
 
-  y -= countStrs * (lineHeight / 2);
-  for (var n = 0; n < countWords; n++) {
-    var
-      tryStr = str + words[n] + " ",
-      tryWidth = context.measureText(tryStr).width;
+        for (var i = 0; i < 2; i++) {
+            w = ox;
+            drawIMG(imgs[i * 2], 0, 0, imgs[i * 2].naturalWidth, imgs[i * 2].naturalHeight, x, y, w, h);
+            x = ox;
+            w = 600 - w;
+            drawIMG(imgs[i * 2 + 1], 0, 0, imgs[i * 2 + 1].naturalWidth, imgs[i * 2 + 1].naturalHeight, x, y, w, h);
+            x = 0;
+            y = oy;
+            h = 600 - h;
+        }
 
-    if (tryWidth > maxWidth) {
-      context.fillText(str, x, y);
-      str = words[n] + " ";
-      y += lineHeight;
-    }
-    else {
-      str = tryStr;
-    }
-  }
-  context.fillText(str, x, y);
+        var context = canvas.getContext('2d');
+
+        context.fillStyle = 'rgba(0,0,0,0.4)';
+        context.fillRect(0, 0, 600, 600);
+		getQuote();
 }
 
-function drawText() {
-  var context = canvas.getContext('2d');
+function drawIMG(img, sx, sy, swidth, sheight, x, y, width, height) {
+    var ctx = canvas.getContext('2d');
 
-  context.fillStyle = 'azure';
-  context.font = '22pt Segoe UI';
-  context.textAlign = 'center';
-  var
-    x = canvas.width / 2,
-    y = canvas.height / 2 + 11;
+    ctx.drawImage(img, sx, sy, swidth, sheight, x, y, width, height);
+}
 
-  formateStrings(context, quote, x, y, 550, 40);
-  download.style.visibility = 'visible';
+async function getQuote() {
+    var http = new XMLHttpRequest;
+
+    http.open('GET', 'https://cors-anywhere.herokuapp.com/' +
+        'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru', true);
+    http.send();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(http.responseText);
+            quote = JSON.parse(http.responseText)['quoteText'];
+			drawQuote();
+        }
+    }
+}
+
+function cutQuote(context, text, x, y, maxWidth, lineHeight) {
+	var words = text.split(" ");
+    var nWords = words.length;
+    var nRows = Math.floor(context.measureText(text).width / 550);
+    var line = "";
+
+    y -= nRows * (lineHeight / 2);
+    for (var n = 0; n < nWords; n++) {
+        var aprxLine = line + words[n] + " ";
+        var aprxWidth = context.measureText(aprxLine).width;
+
+        if (aprxWidth > maxWidth) {
+            context.fillText(line, x, y);
+            line = words[n] + " ";
+            y += lineHeight;
+        }
+        else {
+            line = aprxLine;
+        }
+    }
+    context.fillText(line, x, y);
+}
+
+function drawQuote() {
+        var context = canvas.getContext('2d');
+
+        context.fillStyle = 'azure';
+        context.font = '22pt Comic Sans MS';
+        context.textAlign = 'center';
+        var x = canvas.width / 2;
+        var y = canvas.height / 2 + 11;
+
+        cutQuote(context, quote, x, y, 550, 35);
+		download.style.visibility = 'visible';
+		next.style.visibility = 'visible';
 }
